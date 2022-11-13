7789d62b205e2b0e931723f2cbcd8a825ab3caf0
Carlos <raulromero_412@hotmail.com>
2022-11-13 00:37:59 -0300

    se agregaron etiquetas de forma dinamica

--- script.js
+++ script.js
@@ -19,9 +19,30 @@ btn.addEventListener("click", function(evento){
 
 // Arrow funcition
 const createTask = (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const valor = input.value;
+    //const task = document.querySelector("[data-task]");
+    const list = document.querySelector("[data-list]")   // seleccionamos la etiqueta <ul> 
+    const task = document.createElement("li")  // necesitamos crear una nueva etiqueta <li> y agregarla a list
+    // para que task no pierda el estilo definido en style necesitamos agregarle una clase
+    task.classList.add('card');
+
+    // creamos un template-String con las etiquetas html combinadas con la var value 
+    const contenido =`  <div>
+                            <i class="far fa-check-square icon"></i>
+                            <span class="task">${valor}</span>
+                        </div>
+                        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    // contenido se compone de  todas las etiquetas html junto con el valor de value(ingreso del usuario)
+    
+    // agregamos contenido a la etiqueta <li>, contenida en el objeto task
+    task.innerHTML = contenido;
+
+    // agregamos la etiqueta creada al objeto lista(contenedor)
+    list.appendChild(task) // se añade la etique <li> a list (etiqueta <ul>)
+    input.value = "";
+    console.log(task);
 }
 
 btn.addEventListener("click", createTask); // create Task 
